236c619646ff02c2a4d9a009844d2e1be7dac61c
trung051093 <dotrung051093@gmail.com>
2022-08-06 18:15:06 +0700

    update

--- apps/my-wedding/components/FloatingHeart.tsx
+++ apps/my-wedding/components/FloatingHeart.tsx
@@ -17,7 +17,8 @@ const random = (min = 10, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const generateArrayWithNumber = (num = 0) => Array.from(Array(num).keys());
+const generateArrayWithNumber = (num = 0) =>
+  Array.from(Array(num).keys());
 
 export const FloatingHeart = () => {
   const [hearts, setHearts] = React.useState<Heart[]>([]);
@@ -30,34 +31,40 @@ export const FloatingHeart = () => {
     return num;
   };
 
-  const getHearts = (numberHearts = defaultNumberOfHeart): Heart[] =>
-    generateArrayWithNumber(numberHearts).map((n) => ({
-      id: 'heart-' + n,
-      position: [generatePositionX(), 0],
-    }));
+  const getHearts = React.useCallback(
+    (numberHearts = defaultNumberOfHeart): Heart[] =>
+      generateArrayWithNumber(numberHearts).map((n) => ({
+        id: 'heart-' + n,
+        position: [generatePositionX(), 0],
+      })),
+    []
+  );
 
-  const handleAnimate = async () => {
-    const min = 10;
-    const max = screen?.width;
-    const x = random(min, max);
-    const y = 0;
-    for (const heart of hearts) {
-      animate(
-        `#${heart.id}`,
-        {
-          opacity: [0.8, 0],
-          x: [x, x - 50, x + 50, x, x - 50, x + 50, x],
-          y: [y, screen.availHeight * 2],
-        },
-        {
-          delay: random(1, defaultNumberOfHeart) / 10,
-          duration: random(12, 20),
-          easing: 'ease-in-out',
-          repeat: Infinity,
-        }
-      );
-    }
-  };
+  const handleAnimate = React.useCallback(
+    async (heartArr: Heart[]) => {
+      const min = 10;
+      const max = screen?.width;
+      const x = random(min, max);
+      const y = 0;
+      for (const heart of heartArr) {
+        animate(
+          `#${heart.id}`,
+          {
+            opacity: [0.8, 0],
+            x: [x, x - 50, x + 50, x, x - 50, x + 50, x],
+            y: [y, screen.availHeight * 2],
+          },
+          {
+            delay: random(1, defaultNumberOfHeart) / 10,
+            duration: random(12, 20),
+            easing: 'ease-in-out',
+            repeat: Infinity,
+          }
+        );
+      }
+    },
+    []
+  );
 
   React.useEffect(() => {
     positions.current = generateArrayWithNumber(
@@ -68,12 +75,12 @@ export const FloatingHeart = () => {
   React.useEffect(() => {
     if (!positions.current) return;
     setHearts(getHearts(defaultNumberOfHeart));
-  }, [getHearts, positions]);
+  }, [getHearts]);
 
   React.useEffect(() => {
     if (!hearts.length) return;
-    handleAnimate();
-  }, [hearts, handleAnimate]);
+    handleAnimate(hearts);
+  }, [handleAnimate, hearts]);
 
   return (
     <>
